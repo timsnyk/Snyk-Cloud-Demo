5dc69af99ea9815606ca5f804060f0b18a5a3dd7
Tim Leroy <79591387+timsnyk@users.noreply.github.com>
2023-04-26 15:17:03 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
 #Company Name
 
 #CHANGE THIS - lower case only
-variable "victim_company" {
+variable "im_a_victim" {
   type        = string
   description = "For naming purposes"
-  default     = "testpatch"
+  default     = "testtim"
 }
 
 #CHANGE THIS
 variable "owner" {
   type        = string
   description = "For Tagging and Filtering purposes"
-  default     = "Patch"
+  default     = "Bob"
 }
 
 #AWS Region
